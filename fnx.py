6a2d9b76109648d5e85406c2a192825a99275e4d
Ori Donner <orid@sqreamtech.com>
2017-11-02 18:39:33 +0200

    send sprints summary report -dev version

--- fnx.py
+++ fnx.py
@@ -29,48 +29,11 @@ def main():
     config = import_config()
     connection = conn.postgres_connect(dbname=config['postgres']['dbname'],user=config['postgres']['user'],password=config['postgres']['password'],host=config['postgres']['host'],port=config['postgres']['port'])
     fields_list = conn.get_table_culomns(connection,'vw_userstories_tot')
-    statement = 'select * from vw_userstories_tot'
-    row_list = conn.postgres_rows_select(connection, statement)
-    html = mail.sprints_to_html_table('test',fields_list,row_list)
-    #print html
-    # html = """
-    # <html>
-    #     <head>
-    #         <style>h1 {color: green;text-align: center;}label {color: darkgreen;}table {border-collapse: collapse;width: 100%;}th {text-align: left;padding: 8px;background-color:cornflowerblue;color:white;}.tdErr {color:red;}.tdOk {color:green;}</style>
-    #     </head>
-    #     <body>
-    #         <h1></h1>
-    #         <table>
-    # """ 
-    # html_headers = "<tr>"
-    # for header in fields_list:
-    #     html_headers += "<th>{}</th>".format(header)
-    # html_headers += "</tr>"
+    #statement = 'select * from vw_userstories_tot'
+    #row_list = conn.postgres_rows_select(connection, statement)
+    mail.email_sprints(connection,fields_list)
+    #html = mail.sprints_to_html_table('test',fields_list,row_list)
 
-    # html += html_headers
-   
-
-    # #html_data =""
-    # for row in row_list:
-    #     print row
-    #     set_color = set_status_color(row[10])
-    #     if set_color is not None:
-    #         html_data = '<tr bgcolor="{0}">'.format(set_color)
-    #     else:
-    #         html_data = '<tr>'
-        
-    #     for field in row:
-    #         html_data += "<td>{}</td>".format(field)
-    #     html_data += "</tr>"
-    
-    #     html += html_data
-
-    # html += """
-    #         </table>
-    #     </body>
-    # </html>
-    # """
-    print html
     
 if __name__ == "__main__":
     main()
