4bdd9cb38ca57d01b33b5087ef253c3197ae97b3
Ori Donner <orid@sqreamtech.com>
2017-11-19 12:52:10 +0200

    run automation code with  prerequisites

--- automation/yodiz_pull_all.sh
+++ automation/yodiz_pull_all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LANG=en_GB.UTF-8 source /usr/local/sqream-prerequisites/versions/system-default/bin/use-prerequisites.sh;
+
 yodiz_resources=(
     'users'
     'issues'
@@ -9,7 +11,7 @@ yodiz_resources=(
 )
 #set -x
 for yodiz_resource in ${yodiz_resources[@]};do
-    ./yodiz.py pull -r $yodiz_resource -tr
+    /usr/local/sqream-prerequisites/versions/system-default/bin/python2.7 ./yodiz.py pull -r $yodiz_resource -tr
     sleep 60
 done
 
