ab48bf373c59df5d3360d8ec4e747e681a96c454
Ori Donner <orid@sqreamtech.com>
2017-11-20 18:17:22 +0200

    192.168.0.165 full path for automation

--- automation/send_mail_reports.sh
+++ automation/send_mail_reports.sh
@@ -1,1 +1,1 @@
-./yodiz.py mail --sprints -ls @sprints_mailing_list.txt
+/usr/local/sqream-prerequisites/versions/3.04/bin/python /home/sqream/Documents/Yodiz/yodiz.py mail --sprints -ls @sprints_mailing_list.txt
--- automation/yodiz_pull_all.sh
+++ automation/yodiz_pull_all.sh
@@ -11,7 +11,8 @@ yodiz_resources=(
 )
 #set -x
 for yodiz_resource in ${yodiz_resources[@]};do
-    /usr/local/sqream-prerequisites/versions/system-default/bin/python2.7 ./yodiz.py pull -r $yodiz_resource -tr
+    #/usr/local/sqream-prerequisites/versions/system-default/bin/python2.7 ./yodiz.py pull -r $yodiz_resource -tr
+    /usr/local/sqream-prerequisites/versions/3.04/bin/python /home/sqream/Documents/Yodiz/yodiz.py pull -r $yodiz_resource -tr
     sleep 60
 done
 
