4da570a42524ed377e63e743599b5e906c6407d4
Ori Donner <orid@sqreamtech.com>
2017-11-22 13:56:42 +0200

    set sprints extractor

--- python/general_lib/fnx.py
+++ python/general_lib/fnx.py
@@ -1,6 +1,9 @@
 import email
+import yaml
 import smtplib
+import requests
 import os
+from python.postgres_lib import postgres_connect as conn
 
 def is_key_in_dictionary(d,n1,n2 = None,n3=None):
     if n1 not in d:
@@ -14,9 +17,28 @@ def is_key_in_dictionary(d,n1,n2 = None,n3=None):
 def escape_postgres_string(string):
     return string.replace("'", "''")  
 
-
 def get_full_path(file):
     name = os.path.basename(file)
     full_path = os.path.abspath(file)
     path = full_path.split(name,1)[0]
     return path
+
+def import_config(file):
+    file_path = get_full_path(file)
+    file_name = file_path + '.config'
+    with open(file_name,'r') as config_file:
+        config = yaml.safe_load(config_file)
+    return config
+
+def get_api_response(config,url_headers,url,transact_guid):
+    db_conn = conn.postgres_connect(config)
+    response = requests.get(url,headers=url_headers)
+    lst = response.json()
+    response_code = response.status_code
+    if response_code == 200:
+        response_text = 'Data pull succeeded'
+    else:
+        response_text = response.text
+        # write to log
+    conn.update_api_log(db_conn,transact_guid,url,response_code,response_text)
+    return lst
--- yodiz.py
+++ yodiz.py
@@ -13,40 +13,17 @@ from python.postgres_lib import issues_extractor
 from python.postgres_lib import tasks_extractor
 from python.postgres_lib import userstories_extractor
 from python.general_lib import fnx
-<<<<<<< HEAD
 
-
-def import_config():
-    file_path = fnx.get_full_path(__file__)
-    file_name = file_path + '.config'
-    with open(file_name,'r') as config_file:
-        config = yaml.safe_load(config_file)
-    return config
-
-=======
-
-
-
-
-def import_config():
-    file_path = fnx.get_full_path(__file__)
-    file_name = file_path + '.config'
-    with open(file_name,'r') as config_file:
-        config = yaml.safe_load(config_file)
-    return config
-
->>>>>>> release/v0.1.1-hfx2
 def main():
-    config = import_config()
-    params = arg_parser.params()    
-    connection = conn.postgres_connect(dbname=config['postgres']['dbname'],user=config['postgres']['user'],password=config['postgres']['password'],host=config['postgres']['host'],port=config['postgres']['port'])
+    params = arg_parser.params()
+    config = fnx.import_config(__file__)  
+    connection = conn.postgres_connect(config)
     
     if params.cmd_object == 'build':
         if params.table:
             conn.create_table_objects(connection,params.table)
         if params.show:
             conn.create_all_objects(connection,params.show)
-    
     if params.cmd_object == 'pull':
         transact_guid = uuid.uuid4()
         url_headers={}
@@ -54,9 +31,9 @@ def main():
         url_headers['api-token']= params.token
         table_name = params.resource
         if params.truncate:
-            conn.truncate_table(connection,table_name,transact_guid)
+            conn.truncate_table(config,table_name,transact_guid)
         if params.resource == 'sprints':
-            sprints_extractor.extract(connection,url_headers,transact_guid)
+            sprints_extractor.extract(config,url_headers,transact_guid)
         if params.resource == 'releases':
             releases_extractor.extract(connection,url_headers,transact_guid)
         if params.resource == 'users':
