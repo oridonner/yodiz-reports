55aabeafbca5ccb9166832606b7bfd22d8f6501c
Ori Donner <orid@sqreamtech.com>
2017-11-22 09:49:13 +0200

    set v0.1.1-hfx2 to production

--- automation/yodiz_pull_all.sh
+++ automation/yodiz_pull_all.sh
@@ -11,10 +11,10 @@ yodiz_resources=(
 #set -x
 for yodiz_resource in ${yodiz_resources[@]};do
     # Productiom
-    #/usr/local/sqream-prerequisites/versions/3.04/bin/python /home/sqream/Documents/Yodiz/yodiz.py pull -r $yodiz_resource -tr
+    /usr/local/sqream-prerequisites/versions/3.04/bin/python /home/sqream/Documents/Yodiz/yodiz.py pull -r $yodiz_resource -tr
     
     # Dev\Test
-    /usr/local/sqream-prerequisites/versions/3.04/bin/python /home/orid/Documents/projects/yodiz_new/yodiz.py pull -r $yodiz_resource -tr
+    #/usr/local/sqream-prerequisites/versions/3.04/bin/python /home/orid/Documents/projects/yodiz_new/yodiz.py pull -r $yodiz_resource -tr
     sleep 60
 done
 
@@ -22,5 +22,5 @@ done
 # Production
 /usr/local/sqream-prerequisites/versions/3.04/bin/python /home/sqream/Documents/Yodiz/yodiz.py mail --sprints 
 # Dev\Test
-/usr/local/sqream-prerequisites/versions/3.04/bin/python /home/orid/Documents/projects/yodiz_new/yodiz.py mail --sprints 
+#/usr/local/sqream-prerequisites/versions/3.04/bin/python /home/orid/Documents/projects/yodiz_new/yodiz.py mail --sprints 
 
